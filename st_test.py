66b0c12a940e4acc264ddd3cba32e022259a50a3
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-08-06 22:46:48 +0700

    Update st_test.py

--- st_test.py
+++ st_test.py
@@ -98,7 +98,8 @@ def main():
     import warnings
     warnings.filterwarnings('ignore')
     st.set_option('deprecation.showPyplotGlobalUse', False)
-    st.image('https://drive.google.com/uc?export=view&id=1dWu3kImQ11Q-M2JgLtVz9Dng0MD5S4LK', use_column_width=True)
+    st.image('https://drive.google.com/uc?export=view&id=1uqJad2S9rcwPt-yVvrGIhKbUX-0Gvz0N', use_column_width=True)
+
     st.title('Personal Data Analysis by Datasans')
     st.write('Beta Access.')
     st.write('Beta access diberikan kepada beberapa user sebelum perilisan resmi, mohon digunakan dan berikan input melalui DM akun IG @datasans.book jika ada error atau fitur yang kurang sempurna.')
