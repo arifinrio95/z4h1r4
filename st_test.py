4ed4f6e88b25fc1a364a7da9bd71b754ed364abc
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-08-06 19:54:15 +0700

    Update st_test.py

--- st_test.py
+++ st_test.py
@@ -178,7 +178,7 @@ def main():
                                                     8. Give an explanation for every syntax.
                                                     9. Don’t give me any explanation about the script. Response only with python block.
                                                     10. Do not reload the dataframe.
-                                                    11. Use Try and Except with pass for each syntax.
+                                                    11. Use Try and Except with pass for each syntax, The Title is inside Try and Except.
                                                     12. Gunakan st.write untuk selain visualisasi, dan st.pyplot untuk visualisasi."""}
                 ],
                 max_tokens=14000,
