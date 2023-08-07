636030b410784cfcd027ca080302ebe6dabd3ef6
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-08-07 22:05:02 +0700

    Update st_test.py

--- st_test.py
+++ st_test.py
@@ -29,17 +29,11 @@ from sklearn.cluster import AgglomerativeClustering
 from scipy.cluster.hierarchy import dendrogram, linkage
 from wordcloud import WordCloud
 
-#MainMenu {
-  visibility: hidden;
-}
+hide_github_icon = “”"
 
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#MainMenu {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+“”"
+st.markdown(hide_github_icon, unsafe_allow_html=True
 
 
 warnings.filterwarnings('ignore')
