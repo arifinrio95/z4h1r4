7b389648b0ed90d69220c37a9417792fe3106ff6
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-08-07 19:09:08 +0700

    Update st_test.py

--- st_test.py
+++ st_test.py
@@ -254,7 +254,9 @@ def main():
                 retry_count = 0
                 script = request_prompt(input_pengguna, schema_str, rows_str, error_message, previous_script, retry_count)
                 exec(str(script))
-                
+                st.write("The Script:")
+                st.text(script)
+            
                 # retry_count = 0
                 # error_message = None
                 # previous_script = None
