240cc6e00408f0e6324aac36da9b8b433cefd052
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-08-08 11:28:44 +0700

    Update st_test.py

--- st_test.py
+++ st_test.py
@@ -393,11 +393,12 @@ def show_bar_plot(df):
     if chart_type == 'Single':
         if orientation == 'Vertical':
             sns.barplot(x=column, y=y_columns[0] if y_columns else None, data=df, order=order, color=color_option)
-        else:
+        elif orientation == 'Horizontal':
             if y_columns:
                 sns.barplot(x=y_columns[0], y=column, data=df, order=order, color=color_option)
             else:
                 sns.barplot(y=column, data=df, order=order, color=color_option)  # Plotting the count of categories
+
 
     elif chart_type == 'Grouped':
         for y_col in y_columns:
