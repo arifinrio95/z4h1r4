3ae26fc3f2830335ca7f0d2bb6e039a801531f3c
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-08-10 11:40:37 +0700

    Update st_test.py

--- st_test.py
+++ st_test.py
@@ -804,6 +804,19 @@ def main():
     st.set_page_config(
     layout="wide",
     )
+
+    st.markdown(
+        """
+        <style>
+        .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+        .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+        .viewerBadge_text__1JaDK {
+            display: none;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
     
     import warnings
     warnings.filterwarnings('ignore')
