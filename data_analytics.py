fc5a4cf3a0d1ba823550af4ded8618131b3c874e
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-08-14 14:38:38 +0700

    Update data_analytics.py

--- data_analytics.py
+++ data_analytics.py
@@ -228,7 +228,7 @@ class DataAnalytics():
         x_label_size = plt.xticks()[1][0].get_size()
         y_label_size = plt.yticks()[1][0].get_size()
         if chart_type == 'Grouped':
-            # value = p.get_height() if orientation == 'Vertical' else p.get_width()
+            value = p.get_height() if orientation == 'Vertical' else p.get_width()
             formatted_value = format_value(value)
             
             for p in ax.patches:
