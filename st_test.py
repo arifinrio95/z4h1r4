b0f6bc1ed1856395ec1590c064a63396c803a9c0
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-08-14 20:56:47 +0700

    Update st_test.py

--- st_test.py
+++ st_test.py
@@ -998,10 +998,10 @@ def main():
     # layout="wide",
     # )
 
-    # st.markdown(
-    #     hide_menu,
-    #     unsafe_allow_html=True
-    # )
+    st.markdown(
+        hide_menu,
+        unsafe_allow_html=True
+    )
    
     import warnings
     warnings.filterwarnings('ignore')
