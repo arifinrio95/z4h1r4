220052c022ba0f3adfe0056799cb401cdd732a63
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-08-14 14:37:15 +0700

    Update data_analytics.py

--- data_analytics.py
+++ data_analytics.py
@@ -57,6 +57,7 @@ class DataAnalytics():
         self.missing_df = missing_info.loc[
             missing_info['Total missing values'] > 0]
         st.dataframe(self.missing_df)
+        
     def format_value(value):
         if value_format == 'K':
             value = value / 1_000
@@ -225,7 +226,7 @@ class DataAnalytics():
         x_label_size = plt.xticks()[1][0].get_size()
         y_label_size = plt.yticks()[1][0].get_size()
         if chart_type == 'Grouped':
-            value = p.get_height() if orientation == 'Vertical' else p.get_width()
+            # value = p.get_height() if orientation == 'Vertical' else p.get_width()
             formatted_value = format_value(value)
             
             for p in ax.patches:
