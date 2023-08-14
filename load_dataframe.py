2e7be5a7e7d57f9dbc82183ab541fb84f8891d19
ziyadsyauqi <39609060+ziyadn@users.noreply.github.com>
2023-08-14 10:59:07 +0700

    Create load_dataframe.py

---
+++ load_dataframe.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import csv
+
+class LoadDataframe():
+  
+    def __init__(self, file):
+        self.file = file
+        self.df = None
+
+    def detect_delimiter(self):
+        self.file.seek(0)  # Reset file position to the beginning
+        file_content = self.file.read(1024).decode(errors='ignore')  # Convert bytes to string, ignoring errors
+        delimiter = ","
+        try:
+            dialect = csv.Sniffer().sniff(file_content)
+            delimiter = dialect.delimiter
+        except csv.Error:
+            pass  # keep the delimiter as ","
+        self.file.seek(0)  # Reset file position to the beginning after reading
+        return delimiter
+
+    def load_file_auto_delimiter(self):
+        # Get file extension
+        file_extension = self.file.name.split('.')[-1]  # Assuming the file has an extension
+        if file_extension == 'csv':
+            delimiter = self.detect_delimiter(self.file)
+            self.file.seek(0)  # Reset file position to the beginning
+            self.df = pd.read_csv(self.file, delimiter=delimiter)
+        elif file_extension in ['xls', 'xlsx']:
+            self.df = pd.read_excel(self.file)
+        else:
+            raise ValueError(f'Unsupported file type: {file_extension}')
+        return self.df
