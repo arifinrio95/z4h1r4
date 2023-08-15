16de166c3aea86b6b71e5826b8e50635d9ac1235
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-08-15 14:23:48 +0700

    Update st_test.py

--- st_test.py
+++ st_test.py
@@ -63,7 +63,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("style/style.css")
+local_css("style.css")
 
 ## or use this:
 # .css-ztfqz8 {
