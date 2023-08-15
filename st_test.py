60b274ccc1287c21fb2f2a783d18313239f6fa2f
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-08-15 22:52:12 +0700

    Update st_test.py

--- st_test.py
+++ st_test.py
@@ -1174,25 +1174,21 @@ def main():
                 save_plot_dir=save_dir
             )
             
-            # Check if the directory has any files
-            if not os.listdir(save_dir):
-                st.write("No files found in the directory.")
-            
             # Display all saved plots in Streamlit
             for plot_file in os.listdir(save_dir):
                 full_path = os.path.join(save_dir, plot_file)
                 
+                # Only process files, skip directories
                 if os.path.isfile(full_path) and plot_file.endswith('.html'):
                     try:
                         with open(full_path, "r") as f:
                             bokeh_html = f.read()
-                            # Check the content of bokeh_html
-                            st.text(bokeh_html[:100])  # Display the first 100 characters
                             st.components.v1.html(bokeh_html, width=1000, height=600)
                     except Exception as e:
                         st.write(f"Error reading '{full_path}': {e}")
                 elif os.path.isdir(full_path):
-                    st.write(f"'{full_path}' is a directory, not a file.")
+                    # If you encounter a directory, print a diagnostic message
+                    st.write(f"'{full_path}' is a directory. Skipping it.")
           
         if st.session_state.get('show_natural_language_exploration', False):
             st.subheader("Natural Language Exploration")
