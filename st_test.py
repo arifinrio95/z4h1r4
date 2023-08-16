89b04b1f58559822a2dd0fc70cc4affecff6af0b
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-08-16 10:22:32 +0700

    Update st_test.py

--- st_test.py
+++ st_test.py
@@ -153,7 +153,7 @@ def request_story_prompt(dict_stats):
         {"role": "system", "content": "Aku akan membuat laporan untukmu."},
         {"role": "user", "content": f"""Buatkan laporan berbentuk insights yang interpretatif dari data berikut:  {dict_stats}. 
         Jika ada pesan error, skip saja tidak usah dijelaskan. Tidak usah dijelaskan bahwa kamu membaca dari dictionary.
-        Tulis dalam 3000 kata. Tambahkan kesimpulan dan potensi dari data. Jelaskan dalam bentuk poin-poin."""}
+        Tulis dalam 3000 kata. Tambahkan kesimpulan dan insights yang aplikatif dalam bisnis. Jelaskan dalam bentuk poin-poin."""}
     ]
 
     response = openai.ChatCompletion.create(
