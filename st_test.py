53994f40aa75e3ce22d5a15b1fe95a18b49e1b19
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-08-19 15:08:12 +0700

    Update st_test.py

--- st_test.py
+++ st_test.py
@@ -173,8 +173,7 @@ def request_story_prompt(schema_str, rows_str, min_viz):
         Every script should start with 'BEGIN_CODE' and end with 'END_CODE'.
         Use df directly; it's been loaded before, do not reload the df, and do not redefine the df.
         Give a clear Plotly title.
-        Provide minimum {min_viz} numbers of insights.
-        Ubah format value du kolom Tanggal dari yang seperti ini '12 Agustus 2023 23:04 WIB', menjadi format date python yang valid."""}
+        Provide minimum {min_viz} numbers of insights."""}
     ]
     # Every script should start with 'BEGIN_CODE' and end with 'END_CODE'.
     #
