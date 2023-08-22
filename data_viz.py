6765029b127540516ed26bf76069ed46b64eddb8
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-08-22 15:45:04 +0700

    Update data_viz.py

--- data_viz.py
+++ data_viz.py
@@ -855,7 +855,7 @@ class DataViz():
             chart_height = 400  # height of the chart
 
             def fill_empty_columns(chart_col_idx, columns):
-                while chart_col_idx % 3 != 0:
+                while chart_col_idx % 3 != 2:
                     columns[chart_col_idx % 3].write("")  # Mengisi dengan konten kosong
                     chart_col_idx += 1
 
