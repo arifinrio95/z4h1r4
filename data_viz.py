1f25aa4ec8406fad776ac208cb0874e48c2759b7
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-08-23 22:55:52 +0700

    Update data_viz.py

--- data_viz.py
+++ data_viz.py
@@ -1399,6 +1399,67 @@ class DataViz():
             left_col, center_col, right_col = st.columns(3)
             columns = [left_col, center_col, right_col]
             chart_col_idx = 0
+
+            def plot_all_line_charts(df, integer_cols, float_cols,
+                                     agg_option, chart_width, chart_height,
+                                     columns, chart_col_idx):
+                for x_col in integer_cols:
+                    for y_col in float_cols:
+                        # Skip plotting if x and y columns are the same
+                        if x_col == y_col:
+                            continue
+
+                        fig = create_line_plot(df, x_col, y_col, agg_option,
+                                               chart_width, chart_height)
+                        columns[chart_col_idx % 3].plotly_chart(fig)
+                        chart_col_idx += 1
+
+                chart_col_idx = 0
+
+            def create_line_plot(df, x_col, y_col, aggregation, chart_width,
+                                 chart_height):
+                if aggregation == 'mean':
+                    df_agg = df.groupby(x_col, as_index=False)[y_col].mean()
+                elif aggregation == 'median':
+                    df_agg = df.groupby(x_col, as_index=False)[y_col].median()
+                else:  # sum
+                    df_agg = df.groupby(x_col, as_index=False)[y_col].sum()
+
+                # Create line plot
+                fig = px.line(df_agg, x=x_col, y=y_col)
+
+                # Apply custom style
+                fig.update_layout(
+                    title={
+                        'text':
+                        f'Line Plot of {y_col} by {x_col}<br>(Aggregated by {aggregation})',
+                        # 'x': 0.5,  # Center the title
+                        # 'xanchor': 'center',
+                        'font': {
+                            'size': 12
+                        }  # Increase font size for title
+                    },
+                    width=chart_width,
+                    height=chart_height,
+                    paper_bgcolor="white",  # set background color to white
+                    plot_bgcolor="white",  # set plot background color to white
+                    xaxis_showgrid=True,  # Show x-axis gridlines
+                    xaxis_gridcolor='rgba(200,200,200,0.2)',  # Lighten gridlines
+                    yaxis_showgrid=True,  # Show y-axis gridlines
+                    yaxis_gridcolor='rgba(200,200,200,0.2)',  # Lighten gridlines
+                    margin=dict(t=40, b=40, l=40,
+                                r=10)  # Add margins to the plot
+                )
+
+                # Update line style
+                fig.update_traces(
+                    line=dict(dash="solid",
+                              width=2.5),  # Set line style and width
+                    marker=dict(size=8),  # Adjust marker size
+                    mode="lines+markers"  # Add markers to the line
+                )
+
+                return fig
             
             chart_width = 300  # width of the chart to fit within the column
             chart_height = 400  # height of the chart
