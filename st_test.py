fd06a941ac82460ea6295958e48942eccfe2d624
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-08-23 17:44:32 +0700

    Update st_test.py

--- st_test.py
+++ st_test.py
@@ -336,7 +336,7 @@ def request_story_prompt(schema_str, rows_str, min_viz, api_model, style='Plotly
         Use df directly; it's been loaded before, do not reload the df, and do not redefine the df.
         Give a clear {style} title.
         Optimize the script for efficiency and minimize the number of lines.
-        Visualize various insights with different chart types that are unique and interesting."""}]
+        Visualize different chart types for each insight that are unique and interesting, and it's {min_viz} most important insights from the data."""}]
     # messages = [{
     #     "role":
     #     "system",
