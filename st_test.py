2e1e30d93bd6c6f95603336c534cde09ba3472c3
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-09-04 00:13:08 +0700

    Update st_test.py

--- st_test.py
+++ st_test.py
@@ -1858,7 +1858,7 @@ def main():
                     ('Altair', 'Seaborn', 'Matplotlib'))
 
                 if library == 'Altair':
-                    library = 'Altair (hconcat and vconcat)'
+                    library = 'Altair (hconcat max 3 charts and vconcat for the rest)'
                 if library == 'Seaborn':
                     library = 'Seaborn (facetgrid)'
                 if library == 'Matplotlib':
