c48b00a6446fdee85aad9e68230ac814b509c264
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-09-03 20:45:11 +0700

    Update st_test.py

--- st_test.py
+++ st_test.py
@@ -1605,7 +1605,7 @@ def main():
 
         # if st.session_state.get('manual_exploration', False):
         with tab_manual:
-            dataviz.visualization()
+            dataviz.visualization(df)
 
         # if st.session_state.get('show_natural_language_exploration', False):
         # if tabs == "Explore Visualization & Insight with UlikData":
